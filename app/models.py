8e6a262d28dbd24c973e394a0a63be7bce648b57
yoichi.baba <la2t.yn@gmail.com>
2020-10-12 17:10:06 +0900

    ファイターマスタ実装
    Signed-off-by: yoichi.baba <la2t.yn@gmail.com>

--- app/models.py
+++ app/models.py
@@ -12,86 +12,30 @@ class Item(models.Model):
     https://docs.djangoproject.com/ja/2.1/ref/models/fields/
     """
 
-    # サンプル項目1 文字列
-    sample_1 = models.CharField(
-        verbose_name='サンプル項目1 文字列',
-        max_length=20,
+    # ファイターID
+    fighter_id = models.CharField(
+        verbose_name='ファイターID',
+        max_length=3,
+        blank=False,
+        null=False,
+    )
+
+    # ファイター名
+    fighter_name = models.CharField(
+        verbose_name='ファイター名',
+        max_length=50,
         blank=True,
         null=True,
     )
 
-    # サンプル項目2 メモ
-    sample_2 = models.TextField(
-        verbose_name='サンプル項目2 メモ',
+    # ファイター名(英語)
+    fighter_name_en = models.CharField(
+        verbose_name='ファイター名(英語)',
+        max_length=50,
         blank=True,
         null=True,
     )
 
-    # サンプル項目3 整数
-    sample_3 = models.IntegerField(
-        verbose_name='サンプル項目3 整数',
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目4 浮動小数点
-    sample_4 = models.FloatField(
-        verbose_name='サンプル項目4 浮動小数点',
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目5 固定小数点
-    sample_5 = models.DecimalField(
-        verbose_name='サンプル項目5 固定小数点',
-        max_digits=5,
-        decimal_places=2,
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目6 ブール値
-    sample_6 = models.BooleanField(
-        verbose_name='サンプル項目6 ブール値',
-    )
-
-    # サンプル項目7 日付
-    sample_7 = models.DateField(
-        verbose_name='サンプル項目7 日付',
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目8 日時
-    sample_8 = models.DateTimeField(
-        verbose_name='サンプル項目8 日時',
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目9 選択肢（固定）
-    sample_9_choice = (
-        (1, '選択１'),
-        (2, '選択２'),
-        (3, '選択３'),
-    )
-
-    sample_9 = models.IntegerField(
-        verbose_name='サンプル項目9_選択肢（固定）',
-        choices=sample_9_choice,
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目9 選択肢（マスタ連動）
-    sample_10 = models.ForeignKey(
-        User,
-        verbose_name='サンプル項目10_選択肢（マスタ連動）',
-        blank=True,
-        null=True,
-        related_name='sample_10',
-        on_delete=models.SET_NULL,
-    )
 
     # 以下、管理項目
 
@@ -137,11 +81,11 @@ class Item(models.Model):
         """
         リストボックスや管理画面での表示
         """
-        return self.sample_1
+        return self.fighter_name
 
     class Meta:
         """
         管理画面でのタイトル表示
         """
-        verbose_name = 'サンプル'
-        verbose_name_plural = 'サンプル'
+        verbose_name = 'ファイター名マスタ'
+        verbose_name_plural = 'ファイター名マスタ'
